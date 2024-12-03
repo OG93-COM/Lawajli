199a8a10af3c7da8dfd9118199a273b158d8a0dd
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-12-04 00:31:31 +0100

    lawajli

--- lawajli-app/app/Components/Header.tsx
+++ lawajli-app/app/Components/Header.tsx
@@ -4,15 +4,15 @@ import headerImg from '../../public/header-car.png'
 
 export default function Header() {
   return (
-    <div className='h-[400px] flex justify-evenly items-center'>
+    <div className='h-[400px] flex justify-center items-center'>
         <div className='mx-4 flex flex-col justify-center items-center'>
             <h1 className='px-4 text-2xl lg:text-4xl font-bold text-white mb-5'>
                 Hello,<br/>Start a new business with Lawajli<br/>Add car and start work!
             </h1>
-            <button className='btn-orange w-full lg:w-[400px] mx-4'>Add Your Car</button>
+            <button className='btn-orange w-full lg:w-[400px] mx-4 hover:scale-105 duration-300'>Add Your Car</button>
         </div>
         <div className='hidden md:flex lg:flex'>
-            <Image src={headerImg} width={500} height={400} alt='header'/>
+            <Image src={headerImg} width={600} height={500} alt='header' className='drop-shadow-md'/>
         </div>
     </div>
   )
