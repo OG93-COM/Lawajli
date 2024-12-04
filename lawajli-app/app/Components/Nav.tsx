ffed88a6d565c6da487b94538fb68dfb110d36a0
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-12-04 17:02:10 +0100

    lawajli

--- lawajli-app/app/Components/Nav.tsx
+++ lawajli-app/app/Components/Nav.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CiUser } from "react-icons/ci";
@@ -7,8 +9,12 @@ import { CiUser } from "react-icons/ci";
 
 
 export default function Nav() {
+
+  const [togglePop , setTogglePop] = useState<boolean>(false)
+  
+
   return (
-    <div className='w-full flex justify-between md:justify-around lg:justify-around items-center p-5 gap-6'>
+    <div className='relative w-full flex justify-between md:justify-around lg:justify-around items-center p-5 gap-6'>
         <Link href={"/"} className='pl-5'>
             <Image src={'/logo.png'} width={140} height={120} alt='logo'/>
         </Link>
@@ -18,10 +24,15 @@ export default function Nav() {
             <li className='hover:text-orange-300 duration-300'><Link href={"/delivery"}>DELIVERY</Link></li>
             <li className='hover:text-orange-300 duration-300'><Link href={"/rent"}>RENT CAR</Link></li>
             <li className='hover:text-orange-300 duration-300'>
-              <Link href={"#"}> <CiUser size={28}/></Link>
+            <CiUser onClick={()=> setTogglePop(!togglePop)} size={28} className='cursor-pointer hover:scale-105 duration-300'/>
             </li>
         </ul>
-        <RxHamburgerMenu className='lg:hidden md:hidden text-white' size={32}/>
+        <RxHamburgerMenu className='lg:hidden md:hidden text-white relative' size={32}/>
+        <div className={`menu-popup ${togglePop ? "lg:block md:block" : ""}`}>
+          <p className='pb-1 hover:text-orange-500 cursor-pointer'>Login</p>
+          <p className='pb-1 hover:text-orange-500 cursor-pointer'>Dashboard</p>
+          <p className='pb-1 hover:text-orange-500 cursor-pointer'>Setting</p>
+        </div>
     </div>
   )
 }
