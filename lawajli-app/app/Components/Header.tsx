082046ff79f8d8a45f5ca4c8ddebac261b355f41
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-12-05 01:21:25 +0100

    lawajli

--- lawajli-app/app/Components/Header.tsx
+++ lawajli-app/app/Components/Header.tsx
@@ -4,7 +4,7 @@ import headerImg from '../../public/header-car.png'
 
 export default function Header() {
   return (
-    <div className='h-[400px] flex justify-center items-center'>
+    <div className='h-[400px] flex justify-center items-center -space-x-14'>
         <div className='mx-4 flex flex-col justify-center items-center'>
             <h1 className='px-4 text-2xl lg:text-4xl font-bold text-white mb-5'>
                 Hello,<br/>Start a new business with Lawajli<br/>Add car and start work!
