5b83c6894d739b61ccccad61468f37f547a88cca
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-12-05 18:50:18 +0100

    lawajli

--- lawajli-app/app/types/index.ts
+++ lawajli-app/app/types/index.ts
@@ -4,3 +4,16 @@ export type TCategory = {
     catImg: string,
     pageLink: string
 }
+
+export type TVehicles = {
+    id: string,
+    vehicleName: string,
+    userName: string,
+    content: string,
+    price: string,
+    location: string,
+    catName: string,
+    imgUrl: string,
+    createdAt: string,
+    userEmail: string,
+}
